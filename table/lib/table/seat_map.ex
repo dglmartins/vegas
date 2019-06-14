be38623472ec450d3b794b7e84becee2bf5c267c
danilomartins <dglmartins@outlook.com>
2019-06-14 14:28:27 -0400

    remove helpers

--- table/lib/table/seat_map.ex
+++ table/lib/table/seat_map.ex
@@ -1,13 +1,7 @@
 defmodule Table.SeatMap do
-  # alias Table.Helpers
-
   def new_empty_table() do
     Enum.reduce(1..10, %{}, fn seat, acc_map ->
       Map.put(acc_map, seat, :empty_seat)
     end)
-
-    # for seat <- 1..10 do
-    #   {Helpers.seat_integer_to_atom(seat), :empty_seat}
-    # end
   end
 end
