69ba113445daa5473d9a62129cee6ff045570b60
danilomartins <dglmartins@outlook.com>
2019-06-17 10:17:30 -0400

    player change status, :active, :sitting_out, :disconnected

--- player/lib/player.ex
+++ player/lib/player.ex
@@ -1,7 +1,17 @@
 defmodule Player do
   defstruct name: nil, chip_count: nil, status: :active
 
+  @accepted_status [:active, :sitting_out, :disconnected]
+
   def new(name, chip_count) do
     %Player{name: name, chip_count: chip_count, status: :active}
+  end
+
+  def change_player_status(%Player{} = player, status) when status in @accepted_status do
+    %{player | status: status}
+  end
+
+  def change_player_status(%Player{} = player, _unaccepted_status) do
+    player
   end
 end
--- player/test/player_test.exs
+++ player/test/player_test.exs
@@ -3,6 +3,18 @@ defmodule PlayerTest do
   doctest Player
 
   test "creates a player" do
-    assert Player.new("Danilo", 200) == %Player{name: "Danilo", chip_count: 200, status: :active}
+    player = Player.new("Danilo", 200)
+    assert player == %Player{name: "Danilo", chip_count: 200, status: :active}
+  end
+
+  test "changes player status" do
+    player = Player.new("Danilo", 200) |> Player.change_player_status(:sitting_out)
+    assert player == %Player{name: "Danilo", chip_count: 200, status: :sitting_out}
+
+    player = Player.change_player_status(player, :unnacepted_status)
+    assert player == %Player{name: "Danilo", chip_count: 200, status: :sitting_out}
+
+    player = Player.change_player_status(player, :active)
+    assert player == %Player{name: "Danilo", chip_count: 200, status: :active}
   end
 end
