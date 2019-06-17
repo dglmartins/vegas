1aede2ade68b4ac3853e227fbfd58ba1156b6cb0
danilomartins <dglmartins@outlook.com>
2019-06-17 09:52:41 -0400

    player microservice

---
+++ player/lib/player.ex
@@ -0,0 +1,18 @@
+defmodule Player do
+  @moduledoc """
+  Documentation for Player.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Player.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
