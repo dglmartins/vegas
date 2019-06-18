cb9659f4931669f5d764700824dd2e8af5504b62
danilomartins <dglmartins@outlook.com>
2019-06-18 12:15:15 -0400

    Hand server, Registry server, Overall supervisor, HandSupervisor, deal_hole_card_call

--- nl_holdem_hand/lib/nl_holdem_hand/state.ex
+++ nl_holdem_hand/lib/nl_holdem_hand/state.ex
@@ -1,4 +1,4 @@
-defmodule NlHoldemHand.Hand do
+defmodule NlHoldemHand.State do
   defstruct pots: [],
             sb: nil,
             bb: nil,
@@ -11,16 +11,25 @@ defmodule NlHoldemHand.Hand do
             seat_map: %{},
             min_raise: nil,
             table_id: nil,
+            hand_id: nil,
             current_bet_round: :pre_flop,
             dealer_seat: nil
 
-  def new(seat_map) do
+  def new(hand_id, table_id, min_bet, ante, seat_map, dealer_seat) do
     seat_map =
       seat_map
       |> filter_empty_seats()
       |> Enum.into(%{})
 
-    %NlHoldemHand.Hand{seat_map: seat_map}
+    %NlHoldemHand.State{
+      hand_id: hand_id,
+      table_id: table_id,
+      bb: min_bet,
+      sb: min_bet / 2,
+      ante: ante,
+      seat_map: seat_map,
+      dealer_seat: dealer_seat
+    }
   end
 
   defp filter_empty_seats(seat_map) do
