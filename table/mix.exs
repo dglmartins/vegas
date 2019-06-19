53f7fc1df19f9b6f04210ccbe04254a22e28adf9
danilomartins <dglmartins@outlook.com>
2019-06-19 14:08:56 -0400

    hand as a dependency of table

--- table/mix.exs
+++ table/mix.exs
@@ -22,8 +22,7 @@ defmodule Table.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:player, path: "../player"},
-      {:card, path: "../card"}
+      {:nl_holdem_hand, path: "../nl_holdem_hand"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
