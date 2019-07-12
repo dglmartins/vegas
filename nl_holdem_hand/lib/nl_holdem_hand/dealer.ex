17ddfe2cf0d3aba91a7e781a5fed233f7d514c53
danilomartins <dglmartins@outlook.com>
2019-07-12 10:34:52 -0400

    nl holdem deal flop, turn and river and tests

--- nl_holdem_hand/lib/nl_holdem_hand/dealer.ex
+++ nl_holdem_hand/lib/nl_holdem_hand/dealer.ex
@@ -7,6 +7,33 @@ defmodule NlHoldemHand.Dealer do
     |> HoleCards.deal([false, false])
   end
 
+  def deal_flop(%{community_cards: []} = table_state) do
+    CommunityCards.deal(table_state, 3)
+  end
+
+  def deal_flop(table_state) do
+    IO.puts("Cannot deal flop with community cards already dealt")
+    table_state
+  end
+
+  def deal_turn(%{community_cards: [_card1, _card2, _card3]} = table_state) do
+    CommunityCards.deal(table_state, 1)
+  end
+
+  def deal_turn(table_state) do
+    IO.puts("Cannot deal turn without 3 community cards already dealt")
+    table_state
+  end
+
+  def deal_river(%{community_cards: [_card1, _card2, _card3, _card4]} = table_state) do
+    CommunityCards.deal(table_state, 1)
+  end
+
+  def deal_river(table_state) do
+    IO.puts("Cannot deal river without 4 community cards already dealt")
+    table_state
+  end
+
   # def post_antes(%State{ante: ante} = hand_state) do
   # end
   #
--- nl_holdem_hand/mix.exs
+++ nl_holdem_hand/mix.exs
@@ -23,7 +23,9 @@ defmodule NlHoldemHand.MixProject do
       {:seat_helpers, path: "../seat_helpers"},
       {:player, path: "../player"},
       {:hand_setup, path: "../hand_setup"},
-      {:hole_cards, path: "../hole_cards"}
+      {:hole_cards, path: "../hole_cards"},
+      {:community_cards, path: "../community_cards"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
