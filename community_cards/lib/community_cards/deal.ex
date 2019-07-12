ace1488e022e9d25fa6fb7948ea0ef92a456036a
danilomartins <dglmartins@outlook.com>
2019-07-11 22:18:56 -0400

    test fix and parameter order

--- community_cards/lib/community_cards/deal.ex
+++ community_cards/lib/community_cards/deal.ex
@@ -7,23 +7,23 @@ defmodule CommunityCards.Deal do
         } = table_state,
         number_of_cards
       ) do
-    {community_cards, deck} = deal(community_cards, deck, number_of_cards)
+    {community_cards, deck} = deal(community_cards, number_of_cards, deck)
     %{table_state | community_cards: community_cards, deck: deck, status: :action_to_open}
   end
 
-  def deal(%{status: status} = table_state) do
+  def deal(%{status: status} = table_state, _number_of_cards) do
     IO.puts("Cannot deal community cards when status is #{to_string(status)}")
     table_state
   end
 
-  def deal(community_cards, deck, 0 = _number_of_cards_left_to_deal) do
+  def deal(community_cards, 0 = _number_of_cards_left_to_deal, deck) do
     {community_cards, deck}
   end
 
-  def deal(community_cards, [first_card | rest_of_deck], number_of_cards_left_to_deal)
+  def deal(community_cards, number_of_cards_left_to_deal, [first_card | rest_of_deck])
       when number_of_cards_left_to_deal > 0 do
     community_cards = community_cards ++ [Card.show(first_card)]
     number_of_cards_left_to_deal = number_of_cards_left_to_deal - 1
-    deal(community_cards, rest_of_deck, number_of_cards_left_to_deal)
+    deal(community_cards, number_of_cards_left_to_deal, rest_of_deck)
   end
 end
--- community_cards/test/community_cards_test.exs
+++ community_cards/test/community_cards_test.exs
@@ -17,10 +17,22 @@ defmodule CommunityCardsTest do
 
     assert Enum.count(table_state.community_cards) == 1
 
-    assert show = true
+    assert show == true
     assert table_state.status == :action_to_open
 
     assert Enum.count(table_state.deck) == 51
+  end
+
+  test "does not when status incorrect" do
+    table_state =
+      %{@table_state | status: :dealing_hole_cards}
+      |> CommunityCards.deal(1)
+
+    assert Enum.count(table_state.community_cards) == 0
+
+    assert table_state.status == :dealing_hole_cards
+
+    assert Enum.count(table_state.deck) == 52
   end
 
   test "deals 3 community cards" do
@@ -31,9 +43,9 @@ defmodule CommunityCardsTest do
     [%{show: show_one}, %{show: show_two}, %{show: show_three}] = table_state.community_cards
 
     assert Enum.count(table_state.community_cards) == 3
-    assert show_one = true
-    assert show_two = true
-    assert show_three = true
+    assert show_one == true
+    assert show_two == true
+    assert show_three == true
 
     assert table_state.status == :action_to_open
 
@@ -83,10 +95,10 @@ defmodule CommunityCardsTest do
       %{show: show_five}
     ] = table_state.community_cards
 
-    assert show_one = true
-    assert show_two = true
-    assert show_three = true
-    assert show_four = true
-    assert show_five = true
+    assert show_one == true
+    assert show_two == true
+    assert show_three == true
+    assert show_four == true
+    assert show_five == true
   end
 end
